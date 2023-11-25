1616cc1e244f6ab351357403aff691b59a8a2d0f
waleed2000x <waleedahmed2000x@gmail.com>
2023-11-25 14:33:58 +0500

    Copy & Tick icon | Toggle

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,14 +1,35 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+
 export default function Books() {
   const [books, setBooks] = useState([]);
+  const [iconTogglers, setIconTogglers] = useState([]);
+
   useEffect(() => {
     axios.get("http://localhost:8000/books").then((res) => {
       setBooks(res.data.allBooks);
+
+      setIconTogglers(res.data.allBooks.map(() => true));
     });
   }, []);
-  const handleCopyToClipboard = (text) => {
+
+  const handleCopyToClipboard = (text, index) => {
+    setIconTogglers((prevTogglers) => {
+      const newTogglers = [...prevTogglers];
+      newTogglers[index] = false;
+      return newTogglers;
+    });
+
+    setTimeout(() => {
+      setIconTogglers((prevTogglers) => {
+        const newTogglers = [...prevTogglers];
+        newTogglers[index] = true;
+        return newTogglers;
+      });
+    }, 1000);
+
     navigator.clipboard.writeText(text);
   };
 
@@ -16,7 +37,7 @@ export default function Books() {
     <div className="home-parent">
       <div className="book-list">
         <div className="books">
-          {books.map((book) => {
+          {books.map((book, index) => {
             return (
               <div className="book-container" key={book._id}>
                 <h1>{book.name}</h1>
@@ -24,9 +45,13 @@ export default function Books() {
                 <p>{book.publishedYear}</p>
                 <div className="id-copyclip">
                   <sup>{book._id}</sup>
-                  <ContentCopyIcon
-                    onClick={() => handleCopyToClipboard(book._id)}
-                  />
+                  {iconTogglers[index] ? (
+                    <ContentCopyIcon
+                      onClick={() => handleCopyToClipboard(book._id, index)}
+                    />
+                  ) : (
+                    <CheckCircleOutlineIcon />
+                  )}
                 </div>
               </div>
             );
